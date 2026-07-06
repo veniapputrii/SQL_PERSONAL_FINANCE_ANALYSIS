920fdcd45ff54a812d8ae82a30e7be092f59a407
Veni Agustina <61000887+veniapputrii@users.noreply.github.com>
2026-07-07 06:02:03 +0700

    Add files via upload

--- Personal_finance_analysis.sql
+++ Personal_finance_analysis.sql
@@ -1,7 +1,3 @@
-
-
-
-
 CREATE TABLE finance (
 	Date DATE,
 	user_id INT,
@@ -28,8 +24,8 @@ CREATE TABLE finance (
 	financial_stress_level TEXT,
 	actual_savings INT,
 	savings_goal_met INT
-	
 );
+
 select * from finance;
 ALTER TABLE finance
 ADD COLUMN goal_saving_month INT
@@ -45,35 +41,15 @@ select
 	discretionary_spending,
 	essential_spending,
 	CASE
-		WHEN monthly_expense_total > (monthly_income * 1.5) THEN 'CRITICAL RISK'
-		WHEN discretionary_spending > (essential_spending *2) THEN 'HIGH RISK'
+		WHEN monthly_expense_total > monthly_income THEN 'CRITICAL RISK'
+		WHEN discretionary_spending > essential_spending THEN 'HIGH RISK'
 		ELSE 'Normal'
 	END AS risk_flag
 FROM finance
-WHERE monthly_expense_total > (monthly_income * 1.5) 
-   OR discretionary_spending > (essential_spending * 2)
+WHERE monthly_expense_total > monthly_income  
+   OR discretionary_spending > essential_spending
 ORDER BY monthly_expense_total DESC;
 
-WITH UserSpendingStats AS (
-    SELECT 
-        user_id,
-        date,
-        monthly_expense_total,
-        -- Calculates the average expense for this specific user across all months
-        ROUND(AVG(monthly_expense_total) OVER(PARTITION BY user_id), 2) AS user_avg_expense
-    FROM user_finances
-)
-SELECT 
-    user_id,
-    date,
-    monthly_expense_total,
-    user_avg_expense,
-    -- Calculates the percentage increase above their personal average
-    ROUND(((monthly_expense_total - user_avg_expense) / user_avg_expense) * 100, 2) AS pct_above_average,
-    CASE 
-        WHEN monthly_expense_total > (user_avg_expense * 1.60) THEN 'SPIKE FLAG: Spending 60%+ above personal norm'
-        ELSE 'Normal'
-    END AS spending_anomaly_flag
-FROM UserSpendingStats
-WHERE monthly_expense_total > (user_avg_expense * 1.60)
-ORDER BY pct_above_average DESC;
+
+
+
